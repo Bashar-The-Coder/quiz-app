5421533630c42f01a6be0eadfaa8a2583bbc3b73
Bashar <mat197194@gmail.com>
2022-08-17 17:37:11 +0600

    second commit

--- lib/question.dart
+++ lib/question.dart
@@ -1,15 +1,12 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
- 
+  final String questionText;
 
-    String questionText;
-    Question({this.questtionText});
-
+  Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
-
+    return Text(questionText);
   }
 }
